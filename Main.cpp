e8925b81e158d840da189c527aba0cf4573d4045
unknown <ardhisparahitaa@gmail.com>
2023-09-19 11:58:30 +0700

    first commit

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+// Tugas menghitung sisi miring segitiga
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+main()
+{
+  float sisi_miring, alas, tinggi;
+
+  cout << "=========================================\n";
+  cout << "Menghitung sisi miring segitiga siku-siku\n";
+  cout << "=========================================\n";
+  cout << "masukkan nilai alas = ";
+  cin >> alas;
+  cout << "masukkan nilai tinggi = ";
+  cin >> tinggi;
+  sisi_miring = sqrt(pow(alas, 2) + pow(tinggi, 2));
+  cout << "jadi hasilnya adalah = " << sisi_miring;
+  return 0;
+}
